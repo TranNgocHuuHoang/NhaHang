24b9157c528b2f6dbe21438fc624e1c30fc93ef8
quocbao <quocbao27012002@gmail.com>
2023-06-08 16:15:50 +0700

    a

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './user/login/login.component';
 import { RegisterComponent } from './user/register/register.component';
+import { FoodComponent } from './Component/Food/food.component';
+import { ListFoodComponent } from './Component/List-food/list-food.component';
 
 
 const appRoutes: Routes = [
   { path:'',redirectTo:'user/login', pathMatch:'full'},
   { path:'user/login', component: LoginComponent},
   { path:'user/register', component: RegisterComponent},
-  { path:'**', redirectTo:'user/register'}
+  { path:'**', redirectTo:'user/register'},
+  { path:'Food', component: FoodComponent},
+  { path:'ListFood', component: ListFoodComponent},
+
 
 ];
 
